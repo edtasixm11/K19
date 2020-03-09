ccd452c4e45158a4e9288ace544cd386b92d2e03
Eduard Canet <ecanet@localhost.localdomain>
2020-03-09 09:46:58 +0100

    k19

--- k19:hello/install.sh
+++ k19:hello/install.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-echo "host: $(uname -a) $(hostname)" >> /opt/docker/index.html
 uname -a >> /opt/docker/index.html
 hostname >> /opt/dcker/index.html
 cp /opt/docker/index.html /var/www/html/index.html
